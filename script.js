5eb2e1f3ac1eb8825689c5c8b912915379e46e06
unknown <khalidabdi3101@gmail.com>
2024-10-05 05:43:11 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Good Luck 💪🏾
+function pluralize(words) {
+   
+    const wordCount = {};
+
+    
+    for (const word of words) {
+        wordCount[word] = (wordCount[word] || 0) + 1;
+    }
+
+   
+    return words.map(word => {
+       
+        if (wordCount[word] > 1) {
+            return word + 's';
+        }
+        
+        return word;
+    }).filter((value, index, self) => self.indexOf(value) === index); 
+}
+
+
+console.log(pluralize(["cat", "dog", "cat", "mouse"])); 
+console.log(pluralize(["car", "car", "car"])); 
